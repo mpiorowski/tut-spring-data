553afb454558cfa4709ab73b956c96eadf33b434
Mateusz Piórowski <mateuszp@lsnova.pl>
2018-06-28 13:25:28 +0200

    commit, second section, add + del

--- react-springdata/src/main/java/react/springdata/reactspringdata/data/DatabaseLoader.java
+++ react-springdata/src/main/java/react/springdata/reactspringdata/data/DatabaseLoader.java
@@ -17,5 +17,10 @@ public class DatabaseLoader implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
     this.repository.save(new Employee("Frodo", "Baggins", "ring"));
+    this.repository.save(new Employee("Bilbo", "Baggins", "burglar"));
+    this.repository.save(new Employee("Gandalf", "the Grey", "wizard"));
+    this.repository.save(new Employee("Samwise", "Gamgee", "gardener"));
+    this.repository.save(new Employee("Meriadoc", "Brandybuck", "pony rider"));
+    this.repository.save(new Employee("Peregrin", "Took", "pipe smoker"));
   }
 }
