13497cfe0df2a835239668a97be5d8b09a80d8dc
Mateusz Piórowski <mateuszp@lsnova.pl>
2018-07-03 10:31:59 +0200

    start react + spring secuirty

---
+++ react.springsecurity/rss-server/src/main/java/react/springsecurity/rssserver/RssServerApplication.java
@@ -0,0 +1,12 @@
+package react.springsecurity.rssserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RssServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RssServerApplication.class, args);
+	}
+}
---
+++ react.springsecurity/rss-server/src/test/java/react/springsecurity/rssserver/RssServerApplicationTests.java
@@ -0,0 +1,16 @@
+package react.springsecurity.rssserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RssServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
