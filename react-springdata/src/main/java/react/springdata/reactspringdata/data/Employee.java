840c43e6819dc8837a8b5d4ccb5ea47a18568d7d
Mateusz Piórowski <mateuszp@lsnova.pl>
2018-07-03 09:09:33 +0200

    commit, fourth section, security

--- react-springdata/src/main/java/react/springdata/reactspringdata/data/Employee.java
+++ react-springdata/src/main/java/react/springdata/reactspringdata/data/Employee.java
@@ -3,18 +3,15 @@ package react.springdata.reactspringdata.data;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Version;
+import javax.persistence.*;
 
 @Data
 @Entity
 public class Employee {
 
-  @Id
+  private @Id
   @GeneratedValue
-  private Long id;
+  Long id;
 
   private String firstName;
   private String lastName;
@@ -24,12 +21,69 @@ public class Employee {
   @JsonIgnore
   private Long version;
 
-  private Employee() {
+  @ManyToOne
+  private Manager manager;
+
+  @Override
+  public String toString() {
+    return "Employee{" +
+        "id=" + id +
+        ", firstName='" + firstName + '\'' +
+        ", lastName='" + lastName + '\'' +
+        ", description='" + description + '\'' +
+        ", manager=" + manager +
+        '}';
   }
 
-  public Employee(String firstName, String lastName, String description) {
+
+  public Employee(String firstName, String lastName, String description, Manager manager) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.description = description;
+    this.manager = manager;
   }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Manager getManager() {
+    return manager;
+  }
+
+  public void setManager(Manager manager) {
+    this.manager = manager;
+  }
+
+  private Employee() {
+  }
+
 }
